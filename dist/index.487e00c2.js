079668044bc64f160366234c5c4b4ff41020350c
Jean-Christophe Molhant <jean.christophe.molhant@gmail.com>
2021-10-21 16:57:37 +0200

    started more info modal

---
+++ dist/index.487e00c2.js
@@ -0,0 +1,46 @@
+const app = async ()=>{
+    const src = "https://character-database.becode.xyz/characters/";
+    const getData = async ()=>{
+        const data = await fetch(src);
+        return await data.json();
+    };
+    const database = await getData();
+    console.log(database);
+    for (let item of database){
+        let tpl = document.querySelector('#tpl');
+        let clone = document.importNode(tpl.content, true);
+        clone.querySelector('h2').textContent = item.name;
+        clone.querySelector('p').textContent = item.shortDescription;
+        clone.querySelector('img').src = `data:image;base64,${item.image}`;
+        document.querySelector('#cardContainer').appendChild(clone);
+    }
+    const manageChar = (name, shortDescription, description, image, method)=>{
+        const options = {
+            method: method,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: {
+                name: name,
+                shortDescription: shortDescription,
+                description: description,
+                image: image
+            }
+        };
+        fetch(src, options);
+    };
+    function getDataUrl(img) {
+        // Create canvas
+        const canvas = document.createElement('canvas');
+        const ctx = canvas.getContext('2d');
+        // Set width and height
+        canvas.width = img.width;
+        canvas.height = img.height;
+        // Draw the image
+        ctx.drawImage(img, 0, 0);
+        return canvas.toDataURL('image/jpeg');
+    }
+};
+app();
+
+//# sourceMappingURL=index.487e00c2.js.map
