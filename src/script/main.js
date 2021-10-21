17a2038b0983dcbf3005624ca8952dd89a994f3b
Jean-Christophe Molhant <jean.christophe.molhant@gmail.com>
2021-10-21 14:48:19 +0200

    js implementation started git add .git add .

---
+++ src/script/main.js
@@ -0,0 +1,18 @@
+const app = async () => {
+    const src = "https://character-database.becode.xyz/characters/";
+    const getData = async () => {
+        const data = await fetch(src);
+        return await data.json();
+    }
+    const database = await getData();
+    console.log(database);
+    for(let item of database) {
+        let tpl = document.querySelector('#tpl');
+        let clone = document.importNode(tpl.content, true);
+        clone.querySelector('h2').textContent = item.name;
+        clone.querySelector('p').textContent = item.shortDescription;
+        clone.querySelector('img').src = `data:image;base64,${item.image}`;
+        document.querySelector('#cardContainer').appendChild(clone);
+    }
+}    
+app();
