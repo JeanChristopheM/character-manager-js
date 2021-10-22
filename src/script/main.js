54aa772e7d82bbfb0c039bb5f9600835d0c2d721
Jean-Christophe Molhant <jean.christophe.molhant@gmail.com>
2021-10-22 15:02:04 +0200

    working on editing

--- src/script/main.js
+++ src/script/main.js
@@ -2,9 +2,12 @@ import * as dbHandling from './modules/dbHandling.mjs';
 const showdown = window.showdown;
 const converter = new showdown.Converter();
 const app = async () => {
+
     const moreDetailsModal = document.querySelector('#moreInfoModal');
     const database = await dbHandling.getData();
+    const modifyingModal = document.querySelector('#editingModal');
 
+    // DISPLAYING DATABASE WITH TEMPLATES ONTO DOM
     for(let item of database) {
         let tpl = document.querySelector('#tpl');
         let clone = document.importNode(tpl.content, true);
@@ -14,6 +17,7 @@ const app = async () => {
         clone.querySelector('button').id = item.id;
         document.querySelector('#cardContainer').appendChild(clone);
     }
+    // FUNCTION TO GET THE SPECIFIC ITEM FROM THE DATABASE
     const getSpecificPost = (id) => {
         let match;
         for(let item of database) {
@@ -23,6 +27,7 @@ const app = async () => {
         }
         return match;
     }
+    // ADDING THE EVENT LISTENERS ON THE VIEW MORE BUTTONS
     for (let item of document.querySelectorAll('.viewMore')) {
         item.addEventListener('click', () => {
             let id = item.id;
@@ -35,6 +40,7 @@ const app = async () => {
             moreDetailsModal.querySelector('#detailsPicture').src = `data:image;base64,${dbEntry.image}`;
         })
     }
+    // INITIALIZING THE MARKDOWN EDITOR
     const easyMDE = new EasyMDE({
         toolbar: [
             {
@@ -91,7 +97,63 @@ const app = async () => {
         ],
         element: document.getElementById('longDescriptionInput')
     });
-
+    const modifyingMDE = new EasyMDE({
+        toolbar: [
+            {
+                name: "bold",
+                action: EasyMDE.toggleBold,
+                className: "fa fa-bold",
+                title: "Bold",
+            },
+            {
+                name: "italics",
+                action: EasyMDE.toggleItalic,
+                className: "fa fa-italic",
+                title: "Italic"
+            },
+            {
+                name: "heading",
+                action: EasyMDE.toggleHeadingSmaller,
+                className: "fa fa-header",
+                title: "Heading"
+            },
+            "|", // Separator
+            {
+                name: "quote",
+                action: EasyMDE.toggleBlockquote,
+                className: "fa fa-quote-left",
+                title: "Quote"
+            },
+            {
+                name: "unordered-list",
+                action: EasyMDE.toggleUnorderedList,
+                className: "fa fa-list-ul",
+                title: "Unordered List"
+            },
+            {
+                name: "ordered-list",
+                action: EasyMDE.toggleOrderedList,
+                className: "fa fa-list-ol",
+                title: "Ordered List"
+            },
+            {
+                name: "table",
+                action: EasyMDE.drawTable,
+                className: "fa fa-table",
+                title: "Table"
+            },
+            "|",
+            {
+                name: "guide",
+                action: "https://www.markdownguide.org/basic-syntax/",
+                className: "fa fa-question-circle",
+                title: "Guide"
+            }
+            // [, ...]
+        ],
+        element: document.getElementById('modifyingLongDescription')
+    });
+    // EVENT LISTENER TO CHECK IF USER ADDS IMAGE TO NEW CHARACTER MENU
     let imgInput = document.querySelector('#imageInput');
     imgInput.addEventListener('change', function (e) {
         if (e.target.files) {
@@ -118,6 +180,8 @@ const app = async () => {
             
         }
     });
+
+    // HANDLING THE CLICK ON SUBMIT NEW CHAR
     document.querySelector('#submitNewChar').addEventListener('click', () => {
         let newChar = {};
         newChar.name = document.querySelector('#nameInput').value;
@@ -130,9 +194,21 @@ const app = async () => {
         newChar.image = newSrc.slice(22, newSrc.length);
         dbHandling.manageChar(newChar, 'POST');
     });
+    // HANDLING THE DELETE BUTTON ON CHAR DELETION
     document.querySelector('#deleteCharBtn').addEventListener('click', () => {
         let id = moreDetailsModal.children[0].id;
         dbHandling.manageChar(id, 'DELETE');
     })
+
+    document.querySelector('#editBtn').addEventListener('click', () => {
+        console.log("sending modified");
+        let id = moreDetailsModal.children[0].id;
+        let dbEntry = getSpecificPost(id);
+        modifyingModal.querySelector('#modifyingNameTitle').textContent = dbEntry.name;
+        modifyingModal.querySelector('#modifyingName').value = dbEntry.name;
+        modifyingModal.querySelector('#modifyingShortDescription').value = dbEntry.shortDescription;
+        modifyingModal.querySelector('#modifyingLongDescription').textContent = dbEntry.description;
+        modifyingModal.querySelector('#modifyingPreview').src = `data:image;base64,${dbEntry.image}`;
+    });
 }    
 app();
